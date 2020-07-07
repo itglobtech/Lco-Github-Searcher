93e820347b8f2aa2488bf63c3d7b0183e24f6585
SHANKAR DAKOLIA <itglobetech2020@gmail.com>
2020-07-07 21:49:55 +0530

    Upload Project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyA2uoGiq0jhwWvsun1p8rnECPwdcDW6_Jg",
+    authDomain: "github-search-f8621.firebaseapp.com",
+    databaseURL: "https://github-search-f8621.firebaseio.com",
+    projectId: "github-search-f8621",
+    storageBucket: "github-search-f8621.appspot.com",
+    messagingSenderId: "327319898095",
+    appId: "1:327319898095:web:126e8b4e2e3dbde22ed81a",
+    measurementId: "G-CMK6BYB02Z"
+  },
 };
 
 /*
