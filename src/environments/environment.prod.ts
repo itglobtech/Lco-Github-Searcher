878b0440db765b243b146cb13c996df21f107909
SHANKAR DAKOLIA <itglobetech2020@gmail.com>
2020-07-07 22:00:52 +0530

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyA2uoGiq0jhwWvsun1p8rnECPwdcDW6_Jg",
+    authDomain: "github-search-f8621.firebaseapp.com",
+    databaseURL: "https://github-search-f8621.firebaseio.com",
+    projectId: "github-search-f8621",
+    storageBucket: "github-search-f8621.appspot.com",
+    messagingSenderId: "327319898095",
+    appId: "1:327319898095:web:126e8b4e2e3dbde22ed81a",
+    measurementId: "G-CMK6BYB02Z"
+  },
 };
